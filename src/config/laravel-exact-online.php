3c0897322e55a54894522966fb9458538e3a7dd8
Joshua de Gier <info@pendo.nl>
2017-02-14 23:43:55 +0100

    Resources & readme updates

--- src/config/laravel-exact-online.php
+++ src/config/laravel-exact-online.php
@@ -2,7 +2,14 @@
 
 return [
 
+    /**
+     * The Client ID of your Exact Online application.
+     */
     'exact_client_id' => env('EXACT_CLIENT_ID', ''),
+
+    /**
+     * The Client Secret of your Exact Online application.
+     */
     'exact_client_secret' => env('EXACT_CLIENT_SECRET', '')
 
 ];
