311476be44f84c47b8b14cd19f580c55bab13d0a
Pendo <info@pendo.nl>
2017-06-21 12:48:29 +0200

    Update routes.php
    Made the routes file smaller

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,17 +1,7 @@
 <?php
 
-if(config('laravel-exact-online.exact_multi_user')) {
-    Route::group(['middleware' => ['web']], function () {
-        Route::group(['prefix' => 'exact', 'middleware' => 'auth'], function() {
-            Route::get('connect', ['as' => 'exact.connect', 'uses' => 'PendoNL\LaravelExactOnline\Http\Controllers\LaravelExactOnlineController@appConnect']);
-            Route::post('authorize', ['as' => 'exact.authorize', 'uses' => 'PendoNL\LaravelExactOnline\Http\Controllers\LaravelExactOnlineController@appAuthorize']);
-            Route::get('oauth', ['as' => 'exact.callback', 'uses' => 'PendoNL\LaravelExactOnline\Http\Controllers\LaravelExactOnlineController@appCallback']);
-        });
-    });
-} else {
-    Route::group(['prefix' => 'exact'], function() {
-        Route::get('connect', ['as' => 'exact.connect', 'uses' => 'PendoNL\LaravelExactOnline\Http\Controllers\LaravelExactOnlineController@appConnect']);
-        Route::post('authorize', ['as' => 'exact.authorize', 'uses' => 'PendoNL\LaravelExactOnline\Http\Controllers\LaravelExactOnlineController@appAuthorize']);
-        Route::get('oauth', ['as' => 'exact.callback', 'uses' => 'PendoNL\LaravelExactOnline\Http\Controllers\LaravelExactOnlineController@appCallback']);
-    });
-}
+Route::group(['prefix' => 'exact', 'middleware' => config('laravel-exact-online.exact_multi_user') ? ['web','auth'] : ['web'] ], function() {
+    Route::get('connect', ['as' => 'exact.connect', 'uses' => 'PendoNL\LaravelExactOnline\Http\Controllers\LaravelExactOnlineController@appConnect']);
+    Route::post('authorize', ['as' => 'exact.authorize', 'uses' => 'PendoNL\LaravelExactOnline\Http\Controllers\LaravelExactOnlineController@appAuthorize']);
+    Route::get('oauth', ['as' => 'exact.callback', 'uses' => 'PendoNL\LaravelExactOnline\Http\Controllers\LaravelExactOnlineController@appCallback']);
+});
